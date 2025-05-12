b41a82a67f0d1040299e5e7d47ae5398f3f15b42
andre <andrebeet1@gmail.com>
2025-05-12 16:18:58 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from app import app
+from ruz import create_app
 
-if __name__ == "__main__":
-    # Lancer l'application en mode développement
+app = create_app()
+
+if __name__ == '__main__':
     app.run(debug=True)
